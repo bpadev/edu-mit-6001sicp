7e1b94b26f3c0684f2cf5e5af1872a7920f7bea5
Benjamin Arsenault <hi@benjamin.dev>
2021-05-04 19:33:54 -0400

    setting up IDE for lisp, playing with README

---
+++ abstractions-procedures.scm
@@ -0,0 +1,6 @@
+; Computational process - abstract beings that inhabit computers.
+; as they evolve, processes manipulate other abstract things called data.
+; The evolution of a process is directed by a pattern of rules called a program.
+; People create programs to direct processes.
+#lang racket
+(+ 4 5)
