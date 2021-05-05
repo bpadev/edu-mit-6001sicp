a3c270802627af867b824ddfd770ceccd59a190a
Benjamin Arsenault <hi@benjamin.dev>
2021-05-05 09:33:33 -0400

    renamed first section files, finished up to 1.1.7

--- abs-proc-118.scm
+++ abs-proc-118.scm
@@ -4,6 +4,7 @@
 ; People create programs to direct processes.
 #lang racket
 
+
 ; 1.1 The Elements of Programming
 ; All powerful languages have three mechanisms:
 ; (1) primitive expressions - represent the simplest entities the language is concerned with.
@@ -30,6 +31,7 @@
    (+ (- 10 7)
       6)) ; this is called pretty-printing
 
+
 ; 1.1.2 Naming and the Environment
 ; We say the name identifies a variable whose value is the object.
 ; The interpreter must maintain some sort of memory that keeps track of the name-object pairs.
@@ -46,4 +48,38 @@
 (define (sum-of-squares x y)
   (+ (square x) (square y)))
 
+
 ; 1.1.5 The Substitution Model for Procedure Application
+
+(f 5) ; procedure definition, 5 is the actual parameter.
+(sum-of-square (+ a 1) (* a 2)) ; replace formal parameters with 5.
+(sum-of-square (+ 5 1) (* 5 2))
+(+ (square 6) (square 10)) ; substitute values in for the formal parameters in the body of sum-of-square.
+(+ (* 6 6) (* 10 10)) ; reduce
+(+ 36 100) ; reduce
+136 ; result
+
+; Applicative order versus normal order
+; Normal order, fully expand and then reduce.
+; Applicative order, evaluate the arguments and then apply.
+
+
+; 1.1.6 Condtional Expressions and Predicates
+; primtive predicates, =, >, <
+; (e) is the consequent expression
+(define (abs x)
+  (cond ((> x 0) x)
+        ((= x 0) 0)
+        ((< x 0) (- x))))
+
+(cond ((p1) (e1))
+      ((p2) (e2))
+      .
+      .
+      .
+      ((pn) (en))) ; general form of a conditional expression.
+
+(if (predicate) (consequent) (alternative)) ; if expression
+(and (e)) ; compound predicates
+(not (e))
+(or (e))
