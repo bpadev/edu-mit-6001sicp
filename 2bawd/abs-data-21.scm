72334649085f4aecc49686c0a12b8a103d498968
Benjamin Arsenault <hi@benjamin.dev>
2021-07-22 00:17:02 -0400

    chapter 2 start

---
+++ 2bawd/abs-data-21.scm
@@ -0,0 +1,6 @@
+; 2.1 Introduction to Data Abstraction
+; Data abstraction is a methodology that enables us to isolate how a compound data object is used from the details of how it is constructed from more primitive data objects.
+; Pairs allow us to construct a compound data object, we can access this object using car, cdr.
+
+
+;  
