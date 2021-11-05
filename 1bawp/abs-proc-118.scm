29152baa24f9c27af8acc49c624a801ce1a81fe4
Benjamin Arsenault <hi@benjamin.dev>
2021-11-04 23:01:04 -0400

    Note shift

--- 1bawp/abs-proc-118.scm
+++ 1bawp/abs-proc-118.scm
@@ -1,8 +1,7 @@
-; Computational process - abstract beings that inhabit computers.
-; as they evolve, processes manipulate other abstract things called data.
+; Introduction
+; Computational process - abstract beings that inhabit computers, as they evolve, processes manipulate other abstract things called data.
 ; The evolution of a process is directed by a pattern of rules called a program.
 ; People create programs to direct processes.
-#lang racket
 
 
 ; 1.1 The Elements of Programming
@@ -14,7 +13,6 @@
 (+ (- 45 34) (* 6 7))
 ; (3) means of abstraction - compound elements can be named and manipulated as units.
 (define age 22)
-
 ;In programming, we deal with two kinds of elements: procedures and data.
 
 
@@ -36,11 +34,12 @@
 ; We say the name identifies a variable whose value is the object.
 ; The interpreter must maintain some sort of memory that keeps track of the name-object pairs.
 ; This memory is called the environment (global environment)
-
 (define size 2)
 
 
 ; 1.1.3 Evaluating Combinations
+
+
 ; 1.1.4 Compound Procedures
 (define (function-name parameter) (function_body))
 (define (square x) (* x x)) ; this is a compound procedure. (define ((name) (formal paramaters)) (body))
@@ -51,7 +50,6 @@
 
 
 ; 1.1.5 The Substitution Model for Procedure Application
-
 (f 5) ; procedure definition, 5 is the actual parameter.
 (sum-of-square (+ a 1) (* a 2)) ; replace formal parameters with 5.
 (sum-of-square (+ 5 1) (* 5 2))
@@ -59,7 +57,6 @@
 (+ (* 6 6) (* 10 10)) ; reduce
 (+ 36 100) ; reduce
 136 ; result
-
 ; Applicative order versus normal order
 ; Normal order, fully expand and then reduce.
 ; Applicative order, evaluate the arguments and then apply.
@@ -87,18 +84,15 @@
 
 
 ; 1.1.7 Square Roots by Newton's Method
-; The contrast between function and procedure is a reflection of the general distinction...
-; between describing properties of things and describing how to do things. (declarative knowledge vs. imperative knowledge).
+; The contrast between function and procedure is a reflection of the general distinction between describing properties of things and describing how to do things. (declarative knowledge vs. imperative knowledge).
 ; Mathematics we are concerned with (what is) knowledge, computer science we are concerned with (how to) knowledge.
 
 
 ; 1.1.8 Procedures as Black-Box Abstractions
 ; Crucial that each procedure accomplishes an identifiable task that can be used as a module in defining other procedures.
 ; Black-Box concept revolves around us not being concerned with how the procedure computes its result, only the fact it does it's job.
-
 ; Block structure, allowing a procedure to have internal definitions (functions) that are local to that procedure.
-; Lexical scoping, dictates that free variables in a procedure are taken to refer to bindings made by enclosing procedure defintions;
-; that is, they are looked up in the environment in which the procedure was defined...
+; Lexical scoping, dictates that free variables in a procedure are taken to refer to bindings made by enclosing procedure defintions, that is, they are looked up in the environment in which the procedure was defined.
 
 
 
