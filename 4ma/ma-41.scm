3ec4efe365a880fce93451c62a22d82fd481deec
Benjamin Arsenault <hi@benjamin.dev>
2021-11-09 10:23:31 -0500

    Up until 4.1.6

--- 4ma/ma-41.scm
+++ 4ma/ma-41.scm
@@ -20,4 +20,19 @@
 
 
 ; 4.1.3 Evaluator Data Structures
+; The evaluator implementation must also define the data structures that the evaluator manipulates internally, as part of the execution of a program, such as the representation of procedures and environments and the representation of true and false.
+; We represent an environment as a list of frames. Each frame of an environment is represented as a pair of lists.
+
+
+; 4.1.4 Running the Evaluator as a Program
+
+
+; 4.1.5 Data as Programs
+; One operational view of the meaning of a program is that a program is a description of an abstract (perhaps infinitely large) machine. 
+; We can regard the evaluator as a very special machine that takes as input a description of a machine.
+; From this perspective, our evaluator is seen to be a universal machine. It mimics other machines when these are described as Lisp programs.
+; The deep idea here is that any evaluator can emulate any other.
+
+
+; 4.1.6 Internal Definitions
 ; 
